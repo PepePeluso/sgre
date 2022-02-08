4f2fba989f1ba9b25826eded9f3c1ac4bdc7faa7
PepePeluso <pjrobalinol@utn.edu.ec>
2022-02-08 07:55:56 -0500

    add popper

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/js/bootstrap.bundle'
 import 'react-loading-skeleton/dist/skeleton.css'
 import 'jquery/dist/jquery'
+import ''
 import { AppRouter } from "./routes/AppRouter"
 
 function App() {
